76c1cb804f9b598990ea2c924f070f2f76e9b0ea
Humbertico04 <humbertico2004@gmail.com>
2022-12-05 21:36:49 +0100

    notebook

--- Ej1.py
+++ Ej1.py
@@ -5,14 +5,27 @@ import pandas as pd
 ruta = "./imdb_titulos.csv"
 dft = pd.read_csv(ruta, delimiter=",")
 print(dft.head(5))
+print("\n")
 
 #Cargar como dataframe de pandas el csv imdb_elenco.csv y mostrar sus 5 primeros registros
 ruta = "./imdb_elenco.csv"
 dfe = pd.read_csv(ruta, delimiter=",")
 print(dfe.head(5))
+print("\n")
 
 #Mostrar el número de registros del dataframe de titulos
 print("Número de títulos:", len(dft))
+print("\n")
 
 #Mostrar el número de registros del dataframe de elenco
 print("Número de registros en elenco:", len(dfe))
+print("\n")
+
+#Mostrar las 5 peliculas más antiguas del listado de titulos
+print("Las 5 películas más antiguas son:\n", dft.sort_values(by="year").head(5))
+print("\n")
+
+#Mostrar las peliculas que en el titulo tienen la palabra "Dracula". También mostrar el número total de peliculas que coincidan con este requisito
+print("Las películas con Dracula en el nombre son:\n", dft[dft["title"].str.contains("Dracula")])
+print("Número de películas con Dracula en el nombre:", len(dft[dft["title"].str.contains("Dracula")]))
+print("\n")
