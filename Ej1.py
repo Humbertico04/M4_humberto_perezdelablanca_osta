15d8a468228768116f87acfe0cfc63d444fd55f2
Humbertico04 <humbertico2004@gmail.com>
2022-12-07 21:15:46 +0100

    main y modificaciones

--- Ej1.py
+++ Ej1.py
@@ -1,3 +1,5 @@
+from ast import main
+
 #Importa pandas y usa un alias para poder utilizar sus recursos
 import pandas as pd
 
@@ -41,12 +43,12 @@ print("\n")
 
 #Mostrar cual fue la primer pelicula hecha titulada "Romeo and Juliet"
 RyJ=dft[dft["title"].str.contains("Romeo and Juliet")].sort_values(by="year").head(1)
-print(RyJ)
 print("La primera película titulada {} fue estrenada en {}".format(RyJ['title'].iloc[0], RyJ['year'].iloc[0]))
 print("\n")
 
 #Listar todas las peliculas que contengan la palabra "Exorcist" ordenadas de la más vieja a la más reciente
-print(dft[dft["title"].str.contains("Exorcist")].sort_values(by="year").reset_index().drop(['index'], axis=1))
+print("Películas que contienen Exorcist en su nombre ordenadas por antigüedad:\n", 
+dft[dft["title"].str.contains("Exorcist")].sort_values(by="year").reset_index().drop(['index'], axis=1))
 print("\n")
 
 #Mostrar cuantas peliculas fueron hechas en el año 1950
@@ -80,7 +82,12 @@ print(dfe[(dfe["n"]==1) & (dfe["name"]=="Charlton Heston") & (dfe["year"]>=1960)
 print("\n")
 
 #Mostrar cuantos papeles para actores hubo en la década de los 50's
-print("Total papeles para actores en la década de los 50's:", len(dfe[(dfe["n"]==1) & (dfe["year"]>=1950) & (dfe["year"]<=1959)]))
+print("Total papeles para actores en la década de los 50's:", len(dfe[(dfe["type"]=="actor") & (dfe["year"]>=1950) & (dfe["year"]<=1959)]))
 print("\n")
 
 #Mostrar cuantos papeles para actrices hubo en la década de los 50's
+print("Total papeles para actores en la década de los 50's:", len(dfe[(dfe["type"]=="actress") & (dfe["year"]>=1950) & (dfe["year"]<=1959)]))
+print("\n")
+
+if __name__ == "__main__":
+    main()
